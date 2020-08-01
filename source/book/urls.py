374b0789c165d6cd267e214f93b56233fca91a9e
DaryaNov <dasha_novoselova98@mail.ru>
2020-08-01 13:51:35 +0600

    добавление html и css файлов для вывода на страницу и так же указание путей

--- source/book/urls.py
+++ source/book/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, book_create
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('books/add/', book_create)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from webapp.models import Book,STATUS_CHOICES
+
+
+def index_view(request):
+    is_admin = request.GET.get('is_admin', None)
+    if is_admin:
+        data = Book.objects.all()
+    else:
+        data = Book.objects.filter(status='moderated')
+    return render(request, 'index.html', context={
+        'articles': data
+    })
+
+
+
+def book_create(request):
+    if request.method == "GET":
+        return render(request, 'book_create.html', context={
+            'status_choices': STATUS_CHOICES
+        })
+    elif request.method == 'POST':
+        name_author = request.POST.get('name_author')
+        email = request.POST.get('email')
+        text = request.POST.get('text')
+        status = request.POST.get('status')
+        book = Book.objects.create(name_author=name_author, email=email,text=text,status=status)
+        context = {'article': book}
+        return render(request, 'book_view.html', context)
