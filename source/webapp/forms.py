5f2991779cc8d4d251deb6fa9c7711f146bb9f6a
DaryaNov <dasha_novoselova98@mail.ru>
2020-08-01 16:21:57 +0600

    добавили значение по умолчанию в строке добавления

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.validators import EmailValidator
 from .models import STATUS_CHOICES
 
 default_status = STATUS_CHOICES[0][0]
@@ -8,10 +7,7 @@ default_status = STATUS_CHOICES[0][0]
 
 
 class BookForm(forms.Form):
-    name_author = forms.CharField(max_length=100, required=True, label='Имя автора')
-    email =forms.EmailField(validators=EmailValidator,label='Почта')
+    name_author = forms.CharField(max_length=100, required=True, initial='Name',label='Имя автора')
+    email =forms.EmailField(label='Почта',initial='EmailAdress')
     text = forms.CharField(max_length=3000, required=True, label='Текст', widget=forms.Textarea)
-    status = forms.ChoiceField(choices=STATUS_CHOICES, initial=default_status, label='Статус')
-    created_at = forms.DateTimeField(auto_now_add=True, label='Время создания')
-    updated_at = forms.DateTimeField(auto_now=True, label='Время редактирования')
 
