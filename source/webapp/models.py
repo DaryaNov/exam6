9e6c09d9c4df7bc3a6825e7e70ae95486c4aaf35
DaryaNov <dasha_novoselova98@mail.ru>
2020-08-01 16:17:36 +0600

    добавили значение по умолчанию в строке добавления

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -8,8 +8,8 @@ STATUS_CHOICES = [
 
 
 class Book(models.Model):
-    name_author = models.CharField(max_length=100, null=False, blank=False, verbose_name='Имя автора')
-    email = models.EmailField(max_length=100, null=False, blank=False, verbose_name='Почта')
+    name_author = models.CharField(max_length=100, null=False, blank=False,default='Name', verbose_name='Имя автора')
+    email = models.EmailField(max_length=100, null=False, blank=False, default='EmailAdress',verbose_name='Почта')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='active', verbose_name='Статус')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
